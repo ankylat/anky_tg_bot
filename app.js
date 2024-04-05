8fd9898acf0b267690a83bd944d6950e023073e6
jp <jp@jps-MacBook-Air.local>
2024-04-05 12:15:12 -0300

    aloja

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const { askAnky } = require("./lib/ai_bot");
 
 const TelegramBot = require("node-telegram-bot-api");
 
@@ -23,39 +24,19 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 
 // Listen for any kind of message. There are different kinds of
 // messages.
-bot.on("message", (msg) => {
+bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
-
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, "Received your message");
+  if (!msg?.text) return;
+  if (msg?.text?.length > 320) {
+    return bot.sendMessage(
+      msg.chat.id,
+      "that is too much text. please send me less than 320 characters"
+    );
+  }
+  const replyFromAnky = await askAnky(msg.text);
+  if (replyFromAnky.success) {
+    return bot.sendMessage(msg.chat.id, replyFromAnky);
+  } else {
+    return bot.sendMessage(msg.chat.id, "sorry, there was an error");
+  }
 });
-
-// const { askAnky } = require("./lib/ai_bot");
-
-// const TelegramBot = require("node-telegram-bot-api");
-
-// const token = process.env.TELEGRAM_BOT_ANKY_BOT;
-// const url = process.env.WEBHOOK_URL;
-
-// const bot = new TelegramBot(token, {
-//   webHook: { port: process.env.PORT || 443 },
-// });
-
-// bot.setWebHook(`${url}/${token}`);
-
-// bot.on("message", async (msg) => {
-//   const chatId = msg.chat.id;
-//   if (!msg?.text) return;
-//   if (msg?.text?.length > 320) {
-//     return bot.sendMessage(
-//       msg.chat.id,
-//       "that is too much text. please send me less than 320 characters"
-//     );
-//   }
-//   const replyFromAnky = await askAnky(msg.text);
-//   if (replyFromAnky.success) {
-//     return bot.sendMessage(msg.chat.id, replyFromAnky);
-//   } else {
-//     return bot.sendMessage(msg.chat.id, "sorry, there was an error");
-//   }
-// });
