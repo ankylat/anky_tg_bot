554228b4f1664e56a2862f18fcdc8411e2b81e44
jp <jp@jps-MacBook-Air.local>
2024-04-05 12:08:15 -0300

    aloja

--- app.js
+++ app.js
@@ -1,31 +1,61 @@
 require("dotenv").config();
-const { askAnky } = require("./lib/ai_bot");
 
 const TelegramBot = require("node-telegram-bot-api");
 
+// replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.TELEGRAM_BOT_ANKY_BOT;
-const url = process.env.WEBHOOK_URL;
 
-const bot = new TelegramBot(token, {
-  webHook: { port: process.env.PORT || 443 },
-});
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, { polling: true });
 
-bot.setWebHook(`${url}/${token}`);
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
 
-bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
-  console.log("the chat id is: ", chatId);
-  if (!msg?.text) return;
-  if (msg?.text?.length > 320) {
-    return bot.sendMessage(
-      msg.chat.id,
-      "that is too much text. please send me less than 320 characters"
-    );
-  }
-  const replyFromAnky = await askAnky(msg.text);
-  if (replyFromAnky.success) {
-    return bot.sendMessage(msg.chat.id, replyFromAnky);
-  } else {
-    return bot.sendMessage(msg.chat.id, "sorry, there was an error");
-  }
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
 });
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, "Received your message");
+});
+
+// const { askAnky } = require("./lib/ai_bot");
+
+// const TelegramBot = require("node-telegram-bot-api");
+
+// const token = process.env.TELEGRAM_BOT_ANKY_BOT;
+// const url = process.env.WEBHOOK_URL;
+
+// const bot = new TelegramBot(token, {
+//   webHook: { port: process.env.PORT || 443 },
+// });
+
+// bot.setWebHook(`${url}/${token}`);
+
+// bot.on("message", async (msg) => {
+//   const chatId = msg.chat.id;
+//   if (!msg?.text) return;
+//   if (msg?.text?.length > 320) {
+//     return bot.sendMessage(
+//       msg.chat.id,
+//       "that is too much text. please send me less than 320 characters"
+//     );
+//   }
+//   const replyFromAnky = await askAnky(msg.text);
+//   if (replyFromAnky.success) {
+//     return bot.sendMessage(msg.chat.id, replyFromAnky);
+//   } else {
+//     return bot.sendMessage(msg.chat.id, "sorry, there was an error");
+//   }
+// });
