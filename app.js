dd4df44ada0fc67ad53f88ea44d0ab9f9f9f79fa
jp <jp@jps-MacBook-Air.local>
2024-04-05 12:02:14 -0300

    aloja

--- app.js
+++ app.js
@@ -4,10 +4,17 @@ const { askAnky } = require("./lib/ai_bot");
 const TelegramBot = require("node-telegram-bot-api");
 
 const token = process.env.TELEGRAM_BOT_ANKY_BOT;
+const url = process.env.WEBHOOK_URL;
 
-const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token, {
+  webHook: { port: process.env.PORT || 443 },
+});
+
+bot.setWebHook(`${url}/${token}`);
 
 bot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
+  console.log("the chat id is: ", chatId);
   if (!msg?.text) return;
   if (msg?.text?.length > 320) {
     return bot.sendMessage(
